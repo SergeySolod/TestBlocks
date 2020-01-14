1b30909f5e90715d9c920b62b1e809a8941d491f
SergeySolod <48413787+SergeySolod@users.noreply.github.com>
2020-01-14 00:28:07 -0800

    Made random string

--- src/pages/blocks/blocks.js
+++ src/pages/blocks/blocks.js
@@ -1,12 +1,36 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
+import {setBlock} from "../../redux/reducers/blocks-reducer";
 
 const Blocks = (props) => {
-      return (
+
+    const addBlockSimple = () => {
+        const block = {
+            id: (new Date()).getTime(),
+            text: (Math.random().toString(36).substring(2, 15)),
+            color: 'white',
+            take: false
+        };
+        props.setBlock(block)
+    };
+
+    const addBlockComplete = () => {
+        const block = {
+            id: (new Date()).getTime(),
+            text: (Math.random().toString(36).substring(2, 15)),
+            color: 'green',
+            take: false
+        };
+        props.setBlock(block)
+    };
+
+
+    return (
         <div>
-            Blocks
+            <button onClick={addBlockSimple} className="btn btn-primary">Добавить простой блок</button>
+            <button onClick={addBlockComplete} className="btn btn-primary">Добавить сложный блок</button>
         </div>
     )
 }
 
-export default connect()(Blocks);
+export default connect(null, {setBlock})(Blocks);
--- src/redux/reducers/blocks-reducer.js
+++ src/redux/reducers/blocks-reducer.js
@@ -1,18 +1,22 @@
 const ADD_BLOCK = 'TestBlocks/blocks-reducer/ADD_BLOCK';
 
-let initialState = {
-    blocks: []
+let initialState = [{
+    id: 1,
+    text: 'Посмотреть и законспектировать 97 урок по React/Redux',
+    hue: 'white',
+    take: false,
 }
+]
 
-const blocksReducer = (state = initialState, action) => {
-    switch (action.type) {
+const blocksReducer = (state = initialState, {id, text, hue, take, type}) => {
+    switch (type) {
         case ADD_BLOCK:
-            return {...state, blocks: [ ...state.blocks, ...action.block ]}
+            return [...state, {id, text, hue, take}]
         default:
             return state;
     }
 }
 
-export const setBlock = (block) => ({type: ADD_BLOCK, block});
+export const setBlock = (id, text, hue, take) => ({type: ADD_BLOCK, id, text, hue, take});
 
 export default blocksReducer
